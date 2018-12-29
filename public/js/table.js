73a12f1ecc5b05dcb2d20b740f88136e744dcdff
bandoxx <radivoje.pupovac98@gmail.com>
2018-12-29 19:00:50 +0100

    Update table.js

--- public/js/table.js
+++ public/js/table.js
@@ -5,7 +5,6 @@
             $("#shortDesc").val('');
             $("#longDesc").val('');
             $("#actionButton").val('Insert').attr('class','btn btn-primary').attr('style','display: show').attr('onclick',"manageData('insertRow')");
-            $("#modal-edit").attr('style','display: show');
             $("#modal-view").attr('style','display: none');   
             $("#modalTitle").text("Insert data");     
         });
